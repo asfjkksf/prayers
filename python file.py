009b34d053bf565046118d9c4ad1693c3c4b20b1
asfjkksf <natalie.rapicano@gmail.com>
2026-03-10 01:34:14 +0100

    deleted my thought process :D

--- python file.py
+++
@@ -1,9 +0,0 @@
-print('wher') 
-print('should i edit here?')
-print('it is currently 16h36 and supposedly ive been here for 46 minutes (or 11 on hackatime)')
-# so i think what happened is that i committed on vscode, but it didnt
-# update on github, but now i added the repository on github desktop
-# and it seems to have updated on github online as well
-# so this is the second attempt to see if committing on vscode saves my changes on 
-# both github online and the desktop version, without me having to import the files 
-# manually again. it is currently 01h09 and ive been here 2 minutes
