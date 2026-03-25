b571c20461e04f145c035cbabc4fd89402cd748d
asfjkksf <natalie.rapicano@gmail.com>
2026-03-25 01:08:56 +0100

    coded the to-do list following this tutorial "https://www.youtube.com/watch?v=aEIHZDv_23U", but mine still is vastly different. i added a points system (which gives the user a random amount of points, 30 on average, upon completing any task), and a shop to exchange the points. users can also choose to change the rewards in the shop. i am very  very happy with this so far, but it is very annoying to use, so next i will work on adding interactive elements like buttons and maybe sliders. now i sleep

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 # if all goes well, this will be an interactive to-do list with a points system
 # and the points will be exchangeable for rewards, kinda like the Habitica app, kinda like a videogame
 # i never coded before so wish me luck :D 
-
-
- 
--- python file.py
+++ python file.py
@@ -1,39 +1,133 @@
 
+ninety = []
+sixty = []
+thirty =[]
 
-difficulty = int(input('how difficult is this task on a scale of 1-5?:'))
-if difficulty >5:
-    print('difficulty must be on a scale of 1-5!')
-elif difficulty <1:
-    print('difficulty must be on a scale of 1-5!')    
-elif difficulty >=3:
-    print('uhm good luck')
-elif difficulty <=2:
-    print('ur good man dw')
-
-urgency = int(input('how urgent is this task? on a scale of 1-3:'))
-if urgency == 3:
-    print('URGENT GO DO YOUR WORK RUN')
-elif urgency == 2:
-    print('okay dont procrastinate on it too much')
-elif urgency == 1:
-    print('do your important tasks first, but dont forget about this one')
-else: 
-    print('urgency must be on a scale of 1-3! try again')
+tasks = []
+import random
+points = 0
 
 
-difficulty = int(input('how difficult is this task on a scale of 1-5?:'))
-if difficulty >5:
-    print('secret third option!')
-    shop = int(input('do you want to go to the shop? 1=yes, 2=no'))
-    if shop == 1:
-        print('1. you have chosen to go to the shop')
-    elif shop == 2:
-        print('2. no shop')
+
+def addTask():
+  task = input("Add task: ")
+  tasks.append(task)
+  print(f"➢ Task '{task}' added to the list.")
+    
+def listTasks():
+  if not tasks:
+    print("There are no tasks! Woohoo!")
+    print("Add new tasks by choosing '1'")
+  else:
+    print("\n")  
+    print("    ≫To-do≪")
+    for index, task in enumerate(tasks):
+      print(f"{index}. {task}")
+
+
+def addBasicReward():
+  thirty = input("Add your 30pt reward: ")
+  print(f"3. '{thirty}' - 30pts ")
+    
+def addCommonReward():
+  sixty = input("Add your 60pt reward: ")
+  print(f"2. '{sixty}' - 60pts ")
+
+def addRareReward():
+  ninety = input("Add your 90pt reward: ")
+  print(f"1. '{ninety}' - 90pts ")
+
+
+if __name__ == "__main__":
+
+  print("≫To-do list≪")
+  while True:
+    print("\n")
+    print("       MENU")
+    print("╔══════⋆⋅☆⋅⋆══════╗")
+    print("𝟷. Add task        ")
+    print("𝟸. Complete task   ")
+    print("𝟹. Show tasks      ")
+    print("𝟺. Exchange points ")
+    print("𝟻. Exit            ")
+    print("╚══════⋆⋅☆⋅⋆══════╝")
+
+    choice = input("Enter: ")
+    print("\n")
+
+    if (choice == "1"):
+      addTask()
+
+ 
+    elif (choice == "2"):
+      listTasks()
+      taskToComplete = int(input("Which task have you completed? Enter its number: "))
+      if taskToComplete >= 0 and taskToComplete < len(tasks):
+          tasks.pop(taskToComplete)
+          print(f"you had {points} points,")
+          points = points + random.randint(18, 42)
+          print(f"but now you have {points} points! choose 4 to exchange them")
+      else:
+          print(f"➢ Task {taskToComplete} was not found ✗")
+        
+    elif (choice == "3"):
+      listTasks()
+
+  
+    elif (choice == "4"):
+      print("              POINT EXCHANGE")
+      print("╔══════════════════⋆⋅★⋅⋆══════════════════╗")
+      print(f"        Available points:{points}")
+      print(f"𝟷. {ninety} - 90pts ")
+      print(f"𝟸. {sixty} - 60 pts")
+      print(f"𝟹. {thirty} - 30 pts")
+      print("𝟺. Change rewards")
+      print("𝟻. Menu")
+      print("╚══════════════════⋆⋅★⋅⋆══════════════════╝")
+      shopChoice = (input("Enter: "))
+
+
+      if (shopChoice == '1'):
+          if points < 90:
+               print('you do not have enough points! complete tasks to obtain more')
+          else:
+               points = points - 90
+               print(f"you used 90 points and now have {points} points left")
+               print("have fun! you deserve it!!")
+      elif (shopChoice == '2'):
+          if points < 60:
+              print('you do not have enough points! complete tasks to obtain more')
+          else:
+               points = points - 60
+               print(f"you used 60 points and now have {points} points left")
+               print("enjoy your break! you got this :)")
+      elif (shopChoice == '3'):
+          if points < 30:
+              print('you do not have enough points! complete tasks to obtain more')
+          else:
+               points = points - 30
+               print(f"you used 30 points and now have {points} points left")
+               print("remember to stretch and stay hydrated!")
+              
+
+      elif (shopChoice == '4'):
+          election = input("Which reward would you like to change (1-3)?: ")       
+          if (election == '1'):
+              ninety = input('Add your new 90pt reward:')
+          elif (election == '2'):
+              sixty = input('Add your new 60pt reward:')
+          elif (election == '3'):
+              thirty = input('Add your new 30pt reward:')
+          else:
+              print('You have to choose a number between 1 and 3! You are now back to the menu') 
+      else:
+          print('\n')
+
+
+    elif (choice == "5"):
+      break
+        
     else:
-        print('input not valid why does it say this')
-elif difficulty <1:
-    print('difficulty must be on a scale of 1-5!')    
-elif difficulty >=3:
-    print('uhm good luck')
-elif difficulty <=2:
-    print('ur good man dw')   
+      print("Input must be a number between 1 and 5! Please try again")
+
+print("Bye!")  
